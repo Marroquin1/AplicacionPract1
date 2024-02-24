3129c5df580142fe8cf0c820c0ec067e5fe4babf
Marroquin1 <luis.marroquin@catolica.edu.sv>
2024-02-24 02:44:11 -0600

    Agregar archivos de proyecto.

---
+++ AplicacionPract1/Controllers/equiposController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AplicacionPract1.Models;
+using System.Linq;
+
+namespace AplicacionPract1.Properties
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+
+        private readonly equiposContext _equiposContexto;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos select e).ToList();
+
+            if (listadoEquipo.Count() == 0)
+            {
+
+                return NotFound();
+
+            }
+            return Ok(listadoEquipo);
+
+}
+    }
+}
